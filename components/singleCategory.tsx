805d83a59d1caf20331168288e04d57dc7a4b081
oyu <teal.2022.oyuerdene@gmail.com>
2023-04-19 01:27:18 +0800

    category put request, fetcher=>fetcherGet,

--- components/singleCategory.tsx
+++ components/singleCategory.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useState } from 'react';
+import { fetcherDelete, fetcherPut } from '../utils/fetcher';
 
 type MyComponentProps = {
   category: any;
@@ -6,9 +8,12 @@ type MyComponentProps = {
 };
 
 export function SingleCategory({ category, loadCategory }: MyComponentProps) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [text, setText] = useState('');
+
   function handleDelete() {
     if (window.confirm('Устгах уу')) {
-      axios.delete(`http://localhost:8000/categories/${category._id}`).then((res) => {
+      fetcherDelete(`categories/${category._id}`).then((res) => {
         const { status } = res;
         if (status === 200) {
           loadCategory();
@@ -16,13 +21,66 @@ export function SingleCategory({ category, loadCategory }: MyComponentProps) {
       });
     }
   }
+
+  function handleEdit() {
+    setIsVisible(true);
+  }
+
+  const handleCancelClick = () => {
+    setIsVisible(false);
+  };
+
+  const handleSaveClick = () => {
+    fetcherPut(`categories/${category._id}`, { title: text }).then((res) => {
+      const { status } = res;
+      if (status === 200) {
+        setText(text);
+        setIsVisible(false);
+        loadCategory();
+      }
+    });
+  };
   return (
-    <div key={category._id} className="flex">
-      <div>{category.title}</div>
-      <div className="flex">
-        <button>Edit</button>
-        <button onClick={handleDelete}>Delete</button>
+    <>
+      <div key={category._id} className="flex">
+        <div>{category.title}</div>
+        <div className="flex">
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
+        </div>
       </div>
-    </div>
+
+      {isVisible && (
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-10">
+          <div className="bg-white rounded-lg p-6 w-full max-w-md">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-xl font-bold">Ангилал засах</h2>
+              <button className="text-gray-700" onClick={() => setIsVisible(false)}>
+                Хаах
+              </button>
+            </div>
+            <div className="mb-4">
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="input-field"
+                type="text"
+                value={text}
+                onChange={(e) => {
+                  setText(e.target.value);
+                }}
+              />
+            </div>
+            <div className="flex justify-end">
+              <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-4" onClick={handleCancelClick}>
+                Буцах
+              </button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleSaveClick}>
+                Хадгалах
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import Home from '../../components/home';
-import { fetcher } from '../../utils/fetcher';
+import { fetcherGet } from '../../utils/fetcher';
 import { CategoriesNew } from '../../components/categoriesNew';
 import { CategoriesList } from '../../components/categoriesList';
 
 export default function Category(props: any) {
   const [categories, setCategories] = useState();
   useEffect(() => {
-    fetcher(`categories`).then((data) => setCategories(data));
+    fetcherGet(`categories`).then((data) => setCategories(data));
   }, []);
 
   function loadCategory() {
-    fetcher(`categories`).then((data) => setCategories(data));
+    fetcherGet(`categories`).then((data) => setCategories(data));
   }
 
   return (
