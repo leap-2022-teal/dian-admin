ce0a8279a7f6da054f3fd8165702d54405654a2d
XvcleeD <Xvclee2551@gmail.com>
2023-04-20 19:19:46 +0800

    admin product update

--- components/singleProduct.tsx
+++ components/singleProduct.tsx
@@ -1,16 +1,22 @@
+// import { useEffect, useState } from 'react';
+// import { fetcherSingleGet } from '../utils/fetcher';
 
+// export function SingleCategory(props: any) {
+//   const [product, setProduct] = useState(props.product);
 
-export function SingleCategory() {
-  return (
-    <div key={product._id} className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out w-64 mb-5">
-      <img className="w-full object-cover" src={product.imageUrl} alt="Product Image" />
-      <div className="p-3">
-        <h3 className="text-md font-semibold text-gray-800 mb-1">{product.title}</h3>
+//   useEffect(() => {
+//     fetcherSingleGet(`products/${product._id}`).then((data) => setProduct(data));
+//   }, []);
+//   return (
+//     <div key={product._id} className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out w-64 mb-5">
+//       <img className="w-full object-cover" src={product.imageUrl} alt="Product Image" />
+//       <div className="p-3">
+//         <h3 className="text-md font-semibold text-gray-800 mb-1">{product.title}</h3>
 
-        <div className="flex items-center justify-between">
-          <span className="text-gray-900 text-sm font-bold">{product.unitPrice}₮</span>
-        </div>
-      </div>
-    </div>
-  );
-}
+//         <div className="flex items-center justify-between">
+//           <span className="text-gray-900 text-sm font-bold">{product.unitPrice}₮</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
