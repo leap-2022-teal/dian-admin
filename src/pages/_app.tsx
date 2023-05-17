858dad220c42d09bbd2bc158b6b4667ca7d0e4ac
huslee <Xvclee2551@gmail.com>
2023-05-17 18:19:08 +0800

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
-import Layout from '../../components/layout';
 import { UserProvider } from '../../components/userProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <UserProvider>
-        <Layout>
-          <ToastContainer />
-          <Component {...pageProps} />
-        </Layout>
+        <ToastContainer />
+        <Component {...pageProps} />
       </UserProvider>
     </>
   );
