13854f8a01f16cc05d5b6dcf5a8362d1149ccbd7
huslee <Xvclee2551@gmail.com>
2023-05-17 18:16:49 +0800

    leyout versel render

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Layout from '../../components/layout';
 import { ToastContainer } from 'react-toastify';
+import Layout from '../../components/layout';
 import { UserProvider } from '../../components/userProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <UserProvider>
-        <ToastContainer />
-        <Component {...pageProps} />
+        <Layout>
+          <ToastContainer />
+          <Component {...pageProps} />
+        </Layout>
       </UserProvider>
     </>
   );
