1cfa742aaef730f93950a2e683de6b07340c0d40
oyu <teal.2022.oyuerdene@gmail.com>
2023-05-21 16:32:01 +0800

    login image

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,8 @@ import { fetcherLogin } from '../../utils/fetcher';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from 'next/router';
+import LoginImage from '../../public/login.png';
+import Image from 'next/image';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -51,104 +53,103 @@ export default function Login() {
   }, 3000);
 
   return (
-    <>
-      <section className="bg-gray-50 ">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">Sign in to your account</h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                {!error ? (
+    <div className='h-screen w-screen md:flex '>
+      <div className="flex items-center justify-center px-6 py-8 mx-auto md:my-auto max-w-[850px] w-full bg-white rounded-lg lg:shadow lg   ">
+        <div className="p-4 md:space-y-6 md:flex sm:p-8 md:justify-around md:gap-4">
+          <Image src={LoginImage} alt="" className="md:w-1/2" />
+          <div className='md:w-1/2'>
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">Sign in to your account</h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              {!error ? (
+                <div>
                   <div>
-                    <div>
-                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
-                        Your email
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        value={email}
-                        onChange={(e: any) => setEmail(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 "
-                        placeholder="name@gmail.com"
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="••••••••"
-                        value={password}
-                        onChange={(e: any) => setPassword(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5"
-                      />
-                    </div>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={email}
+                      onChange={(e: any) => setEmail(e.target.value)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 "
+                      placeholder="name@gmail.com"
+                    />
                   </div>
-                ) : (
-                  <div className="animate-shake">
-                    <div>
-                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
-                        Your email
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        value={email}
-                        onChange={(e: any) => setEmail(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 "
-                        placeholder="name@gmail.com"
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="••••••••"
-                        value={password}
-                        onChange={(e: any) => setPassword(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5"
-                      />
-                    </div>
+                  <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      value={password}
+                      onChange={(e: any) => setPassword(e.target.value)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5"
+                    />
                   </div>
-                )}
-
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-300 " />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label htmlFor="remember" className="text-gray-500 ">
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <a href="#" className="text-sm font-medium text-purple-600 hover:underline ">
-                    Forgot password?
-                  </a>
                 </div>
-                <button
-                  type="submit"
-                  onClick={handleLogin}
-                  className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-                >
-                  Sign in
-                </button>
-              </form>
-            </div>
+              ) : (
+                <div className="animate-shake">
+                  <div>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={email}
+                      onChange={(e: any) => setEmail(e.target.value)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 "
+                      placeholder="name@gmail.com"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      value={password}
+                      onChange={(e: any) => setPassword(e.target.value)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5"
+                    />
+                  </div>
+                </div>
+              )}
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-300 " />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label htmlFor="remember" className="text-gray-500 ">
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <a href="#" className="text-sm font-medium text-purple-600 hover:underline ">
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                onClick={handleLogin}
+                className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Sign in
+              </button>
+            </form>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 }
